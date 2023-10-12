cb6a407630c233cffba576e059d0d0eaea1192b2
matthiasbuehl <mattbuehl@gmail.com>
2023-10-12 13:08:28 -0700

    brute force works

--- specs/utils.spec.ts
+++ specs/utils.spec.ts
@@ -3,26 +3,16 @@ import { permutations, heapPermutations } from "../src/utils";
 
 describe("permutations", () => {
   it("should permute abc", () => {
-    expect(permutations("abc").sort()).toEqual([
-      "abc",
-      "acb",
-      "bac",
-      "bca",
-      "cab",
-      "cba",
-    ]);
+    expect(permutations("abc").sort()).toEqual(
+      ["abc", "acb", "bac", "bca", "cab", "cba"].sort()
+    );
   });
 });
 
 describe("heapPermutations", () => {
   it("should permute abc", () => {
-    expect(heapPermutations("abc").sort()).toEqual([
-      "abc",
-      "acb",
-      "bac",
-      "bca",
-      "cab",
-      "cba",
-    ]);
+    expect(heapPermutations("abc").sort()).toEqual(
+      ["abc", "acb", "bac", "bca", "cab", "cba"].sort()
+    );
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,38 @@
-import { permutations } from "./utils";
+import allWords from "./allWords";
+import { heapPermutations } from "./utils";
 
-export default function findWords(word: string, allWords: string[]): string[] {
-  return [];
+export default function findWords(
+  word: string,
+  allWords: Set<string>
+): Set<string> {
+  if (word.length === 0) new Set<string>();
+
+  let validWords: string[] = [];
+  let permutations = heapPermutations(word);
+
+  for (let p of permutations) {
+    validWords = validWords.concat(findWordsInPermutation(p, allWords));
+  }
+
+  return new Set(validWords);
 }
+
+function findWordsInPermutation(
+  permutation: string,
+  allWords: Set<string>
+): string[] {
+  let validWords: string[] = [];
+
+  for (let i = 0; i < permutation.length; i++) {
+    for (let j = i + 1; j <= permutation.length; j++) {
+      let potentialWord = permutation.substring(i, j);
+      if (allWords.has(potentialWord)) {
+        validWords.push(potentialWord);
+      }
+    }
+  }
+  return validWords;
+}
+
+let words = findWords("fhdjeugh", allWords);
+console.log(words);
