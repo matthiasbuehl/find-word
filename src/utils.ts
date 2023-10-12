42b5a156befdd478736cc1f5fd3c59f8d2df834d
matthiasbuehl <mattbuehl@gmail.com>
2023-10-11 20:09:30 -0700

    nits

--- src/utils.ts
+++ src/utils.ts
@@ -1,19 +1,16 @@
 export function getPermutations(word: string): string[] {
   if (word.length < 2) return [word];
 
-  let allPermutations = [];
+  let allPermutations: string[] = [];
 
   for (let i = 0; i < word.length; i++) {
     let currentLetter = word[i];
     let otherLetters = word.slice(0, i) + word.slice(i + 1, word.length);
 
     let permutations = getPermutations(otherLetters);
-    // console.log("firstLetter", currentLetter);
-    // console.log("otherLetterrs", otherLetters);
-    // console.log("permutations", permutations);
 
     for (let permutation of permutations) {
-      allPermutations.push(currentLetter.concat(permutation));
+      allPermutations.push(currentLetter + permutation);
     }
   }
 
