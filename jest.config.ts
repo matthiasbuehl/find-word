0e2b2543e83ae760e6183f56abce5beec6cb677c
matthiasbuehl <mattbuehl@gmail.com>
2023-10-12 16:20:53 -0700

    clean up jest config

--- jest.config.ts
+++ jest.config.ts
@@ -4,10 +4,6 @@ const config: Config = {
   verbose: true,
   preset: "ts-jest",
   testEnvironment: "node",
-  //   transform: {
-  //     "^.+\\.ts?$": "ts-jest",
-  //   },
-  //   transformIgnorePatterns: ["<rootDir>/node_modules/"],
 };
 
 export default config;
