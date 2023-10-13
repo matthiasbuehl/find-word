bc65b024651318da70a1d59a65738bacf9b4c946
matthiasbuehl <mattbuehl@gmail.com>
2023-10-12 22:54:31 -0700

    command line arg

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
 import allWords from "./all_words";
 import { findWords } from "./word_search";
 
-let words = findWords("fjdkeug", allWords);
+const letters = process.argv[2]?.split("=")?.[1];
+
+if (!letters) {
+  console.log("Usage: yarn go letters=<letters> e.g. yarn go oogd");
+  process.exit(1);
+}
+
+let words = findWords(letters, allWords);
 console.log(words);
